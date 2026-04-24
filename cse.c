94a21a6874ac60b465358d3c12a4cfa7d508d3bd
programming <afreensabzar66@gmail.com>
2026-04-24 16:50:45 +0500

    area of circle

--- cse.c
+++ cse.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 int main() {
-    int n;
-    printf("enter the numb: ");
-    scanf("%d", &n);
-    if (n%5==0) {
-    printf("numb is divisible by 5");
-    }else {
-        printf("not divisible");
-    }
+    float n;
+    printf("enter radius: ");
+    scanf("%f", &n);
+    float area = 3.14*n*n;
+    printf("area of circle = %f" , area);
+   
        return 0;
 }
