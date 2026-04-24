72c0734fd6e9a47cf67bf2b0182c6737931df59a
programming <afreensabzar66@gmail.com>
2026-04-24 16:59:53 +0500

    swap two numbers

--- cse.c
+++ cse.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int main() {
-    float n;
-    printf("enter radius: ");
-    scanf("%f", &n);
-    float area = 3.14*n*n;
-    printf("area of circle = %f" , area);
-   
+    int a ,b ,temp;
+    printf("enter a : ");
+    scanf("%d" , &a);
+    printf("enter b :");
+    scanf("%d" , &b);
+    temp=a;
+    a=b;
+    b=temp;
+   printf("After swaping , a = %d b = %d" , a,b);
        return 0;
 }
