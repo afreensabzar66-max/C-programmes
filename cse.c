ff785b15be77cfea14210c3300623a0cde9fb171
programming <afreensabzar66@gmail.com>
2026-04-24 17:30:22 +0500

    print even numbs

--- cse.c
+++ cse.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main() {
-    int a ,b ,temp;
-    printf("enter a : ");
-    scanf("%d" , &a);
-    printf("enter b :");
-    scanf("%d" , &b);
-    temp=a;
-    a=b;
-    b=temp;
-   printf("After swaping , a = %d b = %d" , a,b);
-       return 0;
+   int n,i;
+   
+   printf("enter ur limit : ");
+   scanf("%d", &n);
+
+   for(int i=1;i<=n;i++) {
+    if(i%2==0) {  
+       printf( "%d " , i);
+   } 
+}
+return 0;
 }
