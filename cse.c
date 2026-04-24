22f96f3db6a47ad0be9b34c104c4077495471bc4
programming <afreensabzar66@gmail.com>
2026-04-24 16:37:58 +0500

    sum of two numbers

--- cse.c
+++ cse.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 int main() {
     int a ,b;
+
     printf("enter ist numb : ");
     scanf("%d" , &a);
     printf("enter second numb :");
     scanf("%d" , &b);
-     if(a>b){
-    printf("a is largest");
-     }else{
-        printf("b is largest");
-     }
+    printf("sum = %d" , a+b);
        return 0;
 }
