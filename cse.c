ba1578a5ebef9d019103f1ffd24ce397f75523f9
programming <afreensabzar66@gmail.com>
2026-04-25 18:17:03 +0500

    square previous numb

--- cse.c
+++ cse.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int main() {
    int n,i;
-   int exp=1;
+   int numb=2;
    printf("enter ur limit : ");
    scanf("%d", &n);
 
    for(int i=1;i<=n;i++) {
- exp=exp*exp;;
-  printf("exp : %d \n" , exp);
+    numb=numb*numb;
+  printf("new numb : %d \n" , numb);
        
    } 
 
